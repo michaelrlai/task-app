8469b1f2901428f88fe55d2bc722daaeda0d218a
Michael Lai <mrlai7782@gmail.com>
2021-03-04 20:27:49 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Overview from "./components/Overview";
 
 class App extends Component {
   constructor() {
@@ -9,16 +10,37 @@ class App extends Component {
       tasks: [],
     };
   }
+
+  handleChange = (e) => {
+    this.setState({
+      task: e.target.value,
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: "",
+    });
+  };
+
   render() {
     const { task, tasks } = this.state;
 
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmitTask}>
           <label htmlFor="taskInput">Enter task</label>
-          <input type="text" id="taskInput" />
+          <input
+            onChange={this.handleChange}
+            value={this.state.task}
+            type="text"
+            id="taskInput"
+          />
           <button type="submit">Add Task</button>
         </form>
+        <Overview tasks={tasks} />
       </div>
     );
   }
--- src/components/overview.js
+++ src/components/overview.js
@@ -1,5 +1,16 @@
 import React from "react";
+import uniqid from "uniqid";
 
-const Overview = () => {};
+const Overview = (props) => {
+  const { tasks } = props;
+
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return <li key={uniqid()}>{task}</li>;
+      })}
+    </ul>
+  );
+};
 
 export default Overview;
